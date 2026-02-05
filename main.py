f557bb38431eed6aed3cee64ab13d6ab0a5764fb
David Li <dnkyr2007@gmail.com>
2026-02-04 17:25:12 -0800

    Create groups and make player moveable

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -23,7 +28,11 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
+
+        for d in drawable:
+            d.draw(screen)
+
+        updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
